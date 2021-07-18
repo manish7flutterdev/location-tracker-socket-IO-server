91730fc28f364a611140e26e3abee23dac057c5f
Academician7 <bboyacademician@gmail.com>
2021-07-18 12:19:46 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,46 @@
+var express = require("express")
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+
+app.use(express.json())
+var client =  {}
+
+
+io.on("connection",(socket)=>{
+  console.log("connected",socket.id)
+  
+        socket.on("signin",(id)=>{
+         console.log(id)
+         client[id]=socket
+   // console.log(client)
+                                })
+                                
+        socket.on("message",(messy)=>{
+          console.log("This is the message",messy)
+          let reciever = messy.reciever
+          if(client[reciever])
+          {client[reciever].emit("message",messy)}
+          else{
+            console.log("No reciever")
+          }
+          })
+})
+
+
+
+app.route("/").get((req,res)=>{
+    res.json("This is it")
+})
+
+server.listen(3000,"0.0.0.0",()=>{
+    console.log("Server Started",3000)
+})
+
+
+
+
+
+
+
